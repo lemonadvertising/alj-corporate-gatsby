49a8e01d999c31847a61fffff5507166de72a328
@lemoner-rajesh <rajesh@lemonadverts.com>
2023-07-13 10:14:04 +0400

    added config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,29 @@ module.exports = {
   plugins: [{
     resolve: 'gatsby-source-wordpress',
     options: {
-      "url": ""
-    }
+      url: `https://cms.jameelmotors.com/graphql`,
+      headers:{
+          Authorization:"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvc29sdXRpb25jbXMuYWxqaGVhbHRoLmNvbVwvZW5cLyIsImlhdCI6MTY3NDU1MjU2NywibmJmIjoxNjc0NTUyNTY3LCJleHAiOjE2NzQ1NTI4NjcsImRhdGEiOnsidXNlciI6eyJpZCI6IjgifX19.GCNNHjfaYd-rXohKpYiY1N46ES1GRsmd-NMAI06Oh1Q"
+      },
+      auth: {
+          htaccess: {
+              username: "aljadmin",
+              password: "aljadmin",
+          }
+      },  
+      production: {
+          allow404Images: true,
+          allow401Images: true,
+      },      
+      html: {
+          useGatsbyImage: true,
+          generateWebpImages: true,
+          placeholderType:'none',
+          createStaticFiles: true,
+
+      },
+   
+        
+  },
   }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp"]
 };
