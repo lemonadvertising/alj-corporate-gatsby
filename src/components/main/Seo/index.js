9153b10353f99876595cc7220e0c39be2d0a2fac
sathya230 <s.moorthy@lemonhq.io>
2023-08-29 19:52:07 +0530

    seo image

--- src/components/main/Seo/index.js
+++ src/components/main/Seo/index.js
@@ -11,8 +11,8 @@ export default function Seo({title, description, image}) {
                 <title>{title?title:"website"}</title>
                 <meta name="Keywords" content="" />
                 <meta name="Description" content={description?description:""} />
-                <meta property="og:image" content={image?image:"../../common/images/meta/favicon.ico"} />
-                <meta property="og:image:type" content="image/jpg" />
+                <meta property="og:image" content="../../common/images/meta/favicon.ico" />
+                <meta property="og:image:type" content="image/x-icon" />
                 <meta property="og:image:width" content="436" />
                 <meta property="og:image:height" content="228" />
                 <meta property="og:title" content={title?title:""} />
