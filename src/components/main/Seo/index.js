662d9a708a521f91bd00047cf9d5656c29893c25
Shabbir Malekpurwala <malekpurshabbir@gmail.com>
2023-09-05 13:15:03 +0400

    image update

--- src/components/main/Seo/index.js
+++ src/components/main/Seo/index.js
@@ -3,6 +3,7 @@ import {Helmet} from "react-helmet";
 export default function Seo({title, description, image}) {
   return (
     <>
+    {console.log("show me image path",image)}
           <Helmet>
                 <meta charset="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -10,7 +11,7 @@ export default function Seo({title, description, image}) {
                 <title>{title?title:"website"}</title>
                 <meta name="Keywords" content="" />
                 <meta name="Description" content={description?description:""} />
-                <meta property="og:image" content={image?image:""} />
+                <meta property="og:image" content={image} />
                 <meta property="og:image:type" content="image/png" />
                 <meta property="og:image:width" content="436" />
                 <meta property="og:image:height" content="228" />
--- src/templates/page.js
+++ src/templates/page.js
@@ -1,19 +1,55 @@
-import React from 'react'
+
+import React, { useRef } from 'react'
 import { graphql } from "gatsby";
 import Layout from '../components/layout';
 import Slider from "react-slick";
 // import { StaticImage } from "gatsby-plugin-image"
+import active_icon from '../common/images/active_icon.png'
+import agreed_icon from '../common/images/agreed_icon.png'
+import oem_icon from '../common/images/oem_icon.png';
+
 import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image"
 const Page = (props) => {
   const currentPage = props.data.wpPage;
   const spotlight = props.data.allWpSpotlight;
+
+  const sliderRef = useRef(null);
+
+  const playVideo = (videoId) => {
+    const video = document.getElementById(videoId);
+    video.play();
+  };
+
+  const pauseVideo = (videoId) => {
+    const video = document.getElementById(videoId);
+    video.pause();
+  };
+
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    autoplay:false,
+    preload:'no',
+    // Your Slick Slider settings here
+    beforeChange: (oldIndex, newIndex) => {
+      // Pause the video when sliding away from a slide
+      console.log(spotlight?.edges[oldIndex].node.spotlights.spotlight.mimeType, '======')
+      if(spotlight?.edges[oldIndex].node.spotlights.spotlight.mimeType == "video/mp4"){
+        pauseVideo(`video${oldIndex}`);
+      }
+    },
+    afterChange: (currentIndex) => {
+      // Play the video when sliding to a slide
+      console.log(spotlight?.edges[currentIndex].node.spotlights.spotlight.mimeType, '----', currentIndex)
+      if(spotlight?.edges[currentIndex].node.spotlights.spotlight.mimeType == "video/mp4"){
+        playVideo(`video${currentIndex}`);
+      }
+    },
   };
+
   return (
     <>
       <Layout
@@ -22,17 +58,22 @@ const Page = (props) => {
         description={currentPage.seo.opengraphDescription ? currentPage.seo.opengraphDescription : null}
         image={currentPage.featuredImage.node.sourceUrl ? currentPage.featuredImage.node.sourceUrl : null}
       >
-        
+
         <div className='container'>
           <div className='row'>
 
             <div className='col-md-12'>
-              <Slider {...settings}>
-              {spotlight.edges.map((el) => (
-                  <div>
+              <Slider ref={sliderRef} {...settings}>
+                {spotlight?.edges.map((el, newIndex) => (
+                  <div>{
+                  }
                     {el.node.spotlights.youtubeVideo == null ?
-                      // <img src={el.node.spotlights.spotlight.sourceUrl} alt={el.node.spotlights.spotlight.sourceUrl ? el.node.spotlights.spotlight.sourceUrl : "image"} />
-                      null
+                      el.node.spotlights.spotlight.mediaType == "file" ?
+                        <video id={`video${newIndex}`} autoPlay muted src={el.node.spotlights.spotlight.publicUrl} type="video/mp4">
+                          {/* <source  /> */}
+                        </video>
+                        :
+                        <img src={el.node.spotlights.spotlight.sourceUrl} alt={el.node.spotlights.spotlight.sourceUrl ? el.node.spotlights.spotlight.sourceUrl : "image"} />
                       :
                       <div>
                         <iframe
@@ -48,9 +89,10 @@ const Page = (props) => {
                     }
                   </div>
                 ))
-              }
+                }
               </Slider>
             </div>
+
 
             <div className='col-md-12'>
               <div className='heroSection'>
@@ -59,6 +101,40 @@ const Page = (props) => {
             </div>
             <div className='col-md-12'>
               <div dangerouslySetInnerHTML={{ __html: currentPage.content }} />
+            </div>
+
+            <div className='col-md-12 mt-5'>
+
+              <div class="mapMainWrapper">
+                <div class="map-container">
+                  <div id="mapplic"></div>
+
+                  <ul>
+                    <li><span><img src={active_icon} /></span> <p>Active distribution partnerships</p></li>
+                    <li><span><img src={agreed_icon} /></span>  <p>Agreed distribution territories</p></li>
+                    <li> <span><img src={oem_icon} /></span> <p>OEM solution sources</p></li>
+                  </ul>
+                </div>
+              </div>
+              <div className="map-wrapper">
+                <div className="map-container">
+                  <div id="mapplic" className="mapplic-dark"></div>
+                </div>
+              </div>
+
+              <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            {/* <div className="mapbx">
+                                <div id="vmap"></div>
+                            </div> */}
+
+
+
+                        </div>
+                    </div>
+                </div>
+
             </div>
           </div>
         </div>
@@ -102,6 +178,9 @@ export const painPage = graphql`
             subTitle
             spotlight {
               sourceUrl
+              mediaType
+              mimeType
+              publicUrl
             }
             youtubeVideo
           }
@@ -111,3 +190,6 @@ export const painPage = graphql`
   }
 `;
 
+// apps-fileview.texmex_20230817.01_p1
+// page.js
+// Displaying newsDetails.js.
