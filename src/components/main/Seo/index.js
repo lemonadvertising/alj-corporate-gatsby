7af10170a324a9d5c607ec9dde2e937cf3d1f915
@lemoner-rajesh <rajesh@lemonadverts.com>
2023-09-07 17:53:58 +0400

    dummy content

--- src/components/main/Seo/index.js
+++ src/components/main/Seo/index.js
@@ -11,13 +11,13 @@ export default function Seo({title, description, image}) {
                 <link rel="icon" href="../../common/images/meta/favicon.ico" type="image/x-icon"/>
                 <title>{title?title:"website"}</title>
                 <meta name="Keywords" content="" />
-                <meta name="Description" content={description?description:""} />
+                <meta name="Description" content={description?description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a mattis orci. Phasellus euismod at lectus nec scelerisque. Maecenas sed massa a turpis eleifend dignissim. Integer lobortis eros in dui blandit, "} />
                 <meta property="og:image" content={ogShare} />
                 <meta property="og:image:type" content="image/x-icon" />
                 <meta property="og:image:width" content="436" />
                 <meta property="og:image:height" content="228" />
-                <meta property="og:title" content={title?title:""} />
-                <meta property="og:description" content={description?description:""} />
+                <meta property="og:title" content={title?title:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. "} />
+                <meta property="og:description" content={description?description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a mattis orci. Phasellus euismod at lectus nec scelerisque. Maecenas sed massa a turpis eleifend dignissim. Integer lobortis eros in dui blandit, "} />
           </Helmet>
     </>
   );
