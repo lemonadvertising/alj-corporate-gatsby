d3979cb322d0ab6e8df4225af172e0ac368cefda
@lemoner-rajesh <rajesh@lemonadverts.com>
2023-09-08 09:33:58 +0400

    og tags image

--- src/components/main/Seo/index.js
+++ src/components/main/Seo/index.js
@@ -22,22 +22,7 @@ export default function Seo({title, description, image, url}) {
             site_name: "website",
             }
 }
-/>
-    {/* {console.log(image,'***********************************************')}
-          <Helmet>
-                <meta charset="utf-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="../../common/images/meta/favicon.ico" type="image/x-icon"/>
-                <title>{title?title:"website"}</title>
-                <meta name="Keywords" content="" />
-                <meta name="Description" content={description?description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a mattis orci. Phasellus euismod at lectus nec scelerisque. Maecenas sed massa a turpis eleifend dignissim. Integer lobortis eros in dui blandit, "} />
-                <meta property="og:image" content={ogShare} />
-                <meta property="og:image:type" content="image/x-icon" />
-                <meta property="og:image:width" content="436" />
-                <meta property="og:image:height" content="228" />
-                <meta property="og:title" content={title?title:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. "} />
-                <meta property="og:description" content={description?description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a mattis orci. Phasellus euismod at lectus nec scelerisque. Maecenas sed massa a turpis eleifend dignissim. Integer lobortis eros in dui blandit, "} />
-          </Helmet> */}
+/>  
     </>
   );
 }
